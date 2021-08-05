971b2e44fd4ba2ce83c4ae3e50e68a63fb034b15
silver96 <86579017+silver96@users.noreply.github.com>
2021-08-06 02:25:46 +0900

    Update concat_hospital.py

--- concat_hospital.py
+++ concat_hospital.py
@@ -1,13 +1,5 @@
 import pandas as pd
 
-# #중복 제거
-# df_dup = pd.read_csv('./crawling/cleaned_review_2020.csv', index_col=0)
-# print(df_dup.duplicated().sum())
-# df_undup = df_dup.drop_duplicates()
-# print(df_undup.duplicated().sum())
-# df_undup.to_csv('./crawling/cleaned_review_2020.csv')
-# exit()
-
 df = pd.read_csv('./reviews_Hospital_치과.csv', index_col=0)
 print(df.info())
 df.drop_duplicates()
