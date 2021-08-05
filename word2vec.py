9a4274d1fe12ee4fb8989e73ec05e4be060974e6
silver96 <86579017+silver96@users.noreply.github.com>
2021-08-06 02:40:34 +0900

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -17,6 +17,6 @@ embedding_model = Word2Vec(cleaned_tokens, vector_size=100, window=4,
                            min_count=10, workers=4, epochs=100, sg=1)   # vector_size는 몇차원으로 줄일지 지정, window는 CNN의 kernel_size 개념, 앞뒤로 고려하는 단어의 개수를 나타냄
                                                                         # min_count는 출현 빈도가 20번 이상인 경우만 word track에 추가하라는 의미(즉, 자주 안 나오는 단어는 제외)
                                                                         # workers는 cpu 스레드 몇개 써서 작업할 건지 지정, sg는 어떤 알고리즘 쓸건지 지정
-embedding_model.save('./models/word2VecModel_hospital.model')
+embedding_model.save('./models/word2VecModel_hospital_l2.model')
 print(embedding_model.wv.vocab.keys())
 print(len(embedding_model.wv.vocab.keys()))
